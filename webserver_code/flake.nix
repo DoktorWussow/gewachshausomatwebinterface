c1b3eebd62a5a63f0c14c72fffb4eefd2c113ebf
Bendix Sonnenberg <bendix.sonnenberg@gmx.de>
2025-07-14 14:56:05 +0200

    make the flake.nix work for cross compiling to windows

--- webserver_code/flake.nix
+++ webserver_code/flake.nix
@@ -20,9 +20,27 @@
           "rust-src"
           "llvm-tools-preview"
         ];
+        withTargets = with fenix.packages.${system};
+          combine [
+            rustToolchain
+            targets.x86_64-pc-windows-gnu.stable.rust-std
+          ];
       in {
         devShells.default = pkgs.mkShell {
-          buildInputs = [ rustToolchain pkgs.pkg-config ];
+          buildInputs = [
+
+            (pkgs.pkgsCross.mingwW64.stdenv.cc.override ({
+              extraBuildCommands = ''
+                printf '%s' '-L ${pkgs.pkgsCross.mingwW64.windows.mingw_w64_pthreads}/lib' >>$out/nix-support/cc-ldflags
+              '';
+            }))
+            withTargets
+            pkgs.pkg-config
+            pkgs.pkgsCross.mingwW64.stdenv
+            pkgs.pkgsCross.mingwW64.stdenv.cc
+            pkgs.pkgsCross.mingwW64.buildPackages.gcc
+            pkgs.pkgsCross.mingwW64.libpthread-stubs.buildInputs
+          ];
 
         };
       });
